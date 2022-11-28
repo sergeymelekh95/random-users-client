61b1d9b6716a74dc8f350ae5b60fa63a57404761
sergeymelekh95 <sergeymelekh95@mail.ru>
2022-11-28 11:45:35 +0400

    fix first loading

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://random-users-generator-api.onrender.com/api';
-export const BASE_URL = 'http://localhost:3001/api';
+export const BASE_URL = 'https://random-users-generator-api.onrender.com/api';
+// export const BASE_URL = 'http://localhost:3001/api';
